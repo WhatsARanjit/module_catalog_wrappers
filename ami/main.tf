d42e581488142eead7ea910d64702bcb1d99af1f
WhatsARanjit <whatsaranjit@gmail.com>
2020-11-10 21:22:25 -0500

    Fix up

--- ami/main.tf
+++ ami/main.tf
@@ -1,3 +1,12 @@
+# Random number generator
+# Demo purposes - cause TF to run every time
+resource "random_id" "random" {
+  keepers = {
+    uuid = "${uuid()}"
+  }
+  byte_length = 8
+}
+
 module "ami" {
   source  = "app.terraform.io/hashijit/ami/aws"
   version = "0.1.0"
