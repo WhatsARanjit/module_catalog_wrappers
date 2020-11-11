11b7dd7e3f8f4910e3ccf97a68c4361029275bd1
WhatsARanjit <whatsaranjit@gmail.com>
2020-11-10 22:56:18 -0500

    Moving to data source from workspace

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,9 +1,21 @@
-variable "ami_id" {}
+variable "tf_org" {}
+variable "tf_workspace" {}
+
+data "terraform_remote_state" "ami" {
+  backend = "remote"
+
+  config = {
+    organization = var.tf_org
+    workspaces = {
+      name = var.tf_workspace
+    }
+  }
+}
 
 module "ec2sandbox" {
   source  = "app.terraform.io/hashijit/ec2sandbox/aws"
   version = "0.1.0"
-  ami_id  = var.ami_id
+  ami_id  = data.terraform_remote_state.ami.outputs.ami_id
 }
 
 output "public_dns" {
